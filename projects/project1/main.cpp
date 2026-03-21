cb4b73ee3997d2b5f87ba4d520a56873407ab189
mihaibianca24 <bm24082006@gmail.com>
2026-03-21 16:07:54 +0200

    added constructors and destructor for Product

--- projects/project1/main.cpp
+++ projects/project1/main.cpp
@@ -15,4 +15,48 @@ public:
     Product(const Product& obj);
     Product& operator=(const Product& obj);
     ~Product();
+
+    int getId() const;
 };
+int Product::noProducts=0;
+Product::Product() : id(++noProducts) {
+    this->name =new char[4];
+    strcpy(this->name,"N/A");
+    this->price=0.0;
+    this->isAvailable=false;
+    this->stock=0;
+}
+Product::Product(char* name, double price,int stock) : id(++noProducts) {
+    this->name=new char[strlen(name)+1];
+    strcpy(this->name,name);
+    this->price=price;
+    this->stock=stock;
+    this->isAvailable=(stock>0);
+}
+Product::Product(const Product& obj) : id(++noProducts) {
+    this->name=new char[strlen(obj.name)+1];
+    strcpy(this->name,obj.name);
+    this->price=obj.price;
+    this->stock=obj.stock;
+    this->isAvailable=obj.isAvailable;
+}
+Product& Product::operator=(const Product& obj) {
+    if (this==&obj) return *this;
+    delete[] this->name;
+    this->name=new char[strlen(obj.name)+1];
+    strcpy(this->name,obj.name);
+    this->price=obj.price;
+    this->stock=obj.stock;
+    this->isAvailable=obj.isAvailable;
+    return *this;
+}
+Product::~Product() {
+    delete[] this->name;
+}
+
+int Product::getId() const {
+    return id;
+}
+int main() {
+    return 0;
+}
